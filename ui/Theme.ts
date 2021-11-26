33a152c9a0aa4de6f423ac309f8ecc803ee45e0a
Luis Zepeda <luis@picnicdigital.io>
2021-11-26 03:21:47 -0600

    feat(theme): new colors added

--- ui/Theme.ts
+++ ui/Theme.ts
@@ -1,6 +1,10 @@
 const theme = {
   colors: {
     primary: "#F94E09",
+    secondary: "#0A2240",
+    action: "#18609C",
+    white: "#FFFFFF",
+    black: "#000000",
   },
   fonts: {
     primary: "Montserrat",
