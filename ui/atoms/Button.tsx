a43fd9c4519612701ee1ad6d8f51ebd0ebea71fc
Luis Zepeda <luis@picnicdigital.io>
2021-11-27 23:29:22 -0600

    feat(atoms): variants added for button component

--- ui/atoms/Button.tsx
+++ ui/atoms/Button.tsx
@@ -1,15 +1,19 @@
 import styled from "styled-components";
 
-const Button = styled.button`
+const Button = styled.button<{ primary?: boolean; disabled?: boolean }>`
   font-size: 1.2rem;
   padding: 0.5rem 2rem;
   border-radius: 3rem;
   font-weight: 500;
-  color: ${({ theme }) => theme.colors.action};
   font-family: ${({ theme }) => theme.fonts.primary};
-  background: ${({ theme }) => theme.colors.white};
+  color: ${(props) =>
+    props.primary ? props.theme.colors.white : props.theme.colors.action};
+  background: ${(props) =>
+    props.primary ? props.theme.colors.primary : props.theme.colors.white};
+  opacity: ${(props) => (props.disabled ? 0.5 : 1)};
+  cursor: ${(props) => (props.disabled ? "auto" : "pointer")};
 
-  @media (min-width: 768px) {
+  @media (${({ theme }) => theme.device.md}) {
     font-size: 2.4rem;
     padding: 1rem 4rem;
   }
